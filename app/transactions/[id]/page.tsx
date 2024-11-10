48948a96faa77d224edda4275a5b0da2db267e78
Lucas Paiva <lcspaiva87@gmail.com>
2024-11-09 21:37:29 -0300

    fix:Add subscription and transaction pages with color settings in Tailwind

---
+++ app/transactions/[id]/page.tsx
@@ -0,0 +1,4 @@
+export default function Transaction({ params }: { params: { id: string } }) {
+  return <div>Transaction {params.id}</div>;
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
         foreground: "var(--foreground)",
+        background: {
+          "1": "#0F0E11", 
+          "2": "#141414",
+        },
+        darkGrey: "#161716",
+        green: "#55B02E",
+        red: "#E93030",
+        darkGray: "#1F1F21",
+        gray: "#71717A",
+        lightGray: "#B8B8B8",
+        white: "#FFFFFF",
       },
     },
   },
